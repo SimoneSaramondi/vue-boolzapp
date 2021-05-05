66125caafea8164caf508fb1630796245f4cfabb
SimoneSaramondi <simo98sara@gmail.com>
2021-05-05 12:29:13 +0200

    particolari

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
-        //index:0,
         userActive: globalUsersList[0],
         searchText: "",
         newMessage: "",
@@ -12,25 +11,24 @@ const app = new Vue({
             return `img/avatar${userAvatar}.jpg`
         },
         changeActiveUser(indice){
-            //console.log(indice,this);
             this.userActive = this.usersList[indice]
         },
-        formatTime(stringDate){
+       /* formatTime(stringDate){
             return moment(stringDate, "DD/MM/YYYY HH:mm:ss").format("HH:mm")
-        },
+        },*/
         sendMessage(){
 
             if(this.newMessage.trim().length != 0){
 
                 let newMessageObject = {
-                    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                    date: moment('13/08/2020 15:30:55').format('DD/MM/YYYY HH:mm:ss'),
                     text: this.newMessage,
                     status: 'sent'
                 };
 
                 let currentchat = this.globalUsersList[this.userActive].text
                 currentchat.push(newMessageObject);
-                this.newMessage="";
+                this.newMessage= "";
 
             }
         }
